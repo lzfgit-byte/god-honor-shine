2c2442715e6dc784d4f6ca98c82deb69f93da988
18074 <1807409742>
2022-08-22 22:39:46 +0800

    init

--- src/samples/node-api.ts
+++ src/samples/node-api.ts
@@ -3,13 +3,13 @@ import { ipcRenderer } from 'electron';
 ipcRenderer.on('main-process-message', (_event, ...args) => {
   console.log('[Receive Main-process message]:', ...args);
 });
-ipcRenderer
-  .invoke('getHtmlAxios', 'https://thehentaiworld.com/?new')
-  .then((res) => {
-    debugger;
-    console.log(res);
-  })
-  .catch((res) => {
-    debugger;
-    console.log(res);
-  });
+// ipcRenderer
+//   .invoke('getHtmlAxios', 'https://thehentaiworld.com/?new')
+//   .then((res) => {
+//     debugger;
+//     console.log(res);
+//   })
+//   .catch((res) => {
+//     debugger;
+//     console.log(res);
+//   });
