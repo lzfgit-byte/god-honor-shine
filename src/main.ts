3fd60689feb9a2996fcfb430c0cbea1da77ac0ea
18074 <1807409742>
2022-08-22 23:06:54 +0800

    init

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import './samples/node-api';
-
+import 'ant-design-vue/dist/antd.css';
 createApp(App)
   .mount('#app')
   .$nextTick(() => {
