d83da008a978fc75cbb80ff44cc2e201a76f3438
lzfgit-byte <1807409742@qq.com>
2023-04-03 21:14:23 +0800

    init

---
+++ src/rule34/rule34.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="container">
+    <h1></h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import { ref } from 'vue';
+</script>
+
+<style scoped lang="less"></style>
