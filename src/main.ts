b68d6b82eda7f6e3419e848085f6806fa12296df
lzfgit-byte <1807409742@qq.com>
2023-12-31 20:37:33 +0800

    feat：init app

--- src/main.ts
+++ src/main.ts
@@ -9,5 +9,3 @@ import { registerRouter } from '@/router/router';
 const app = createApp(App);
 registerRouter(app);
 app.use(ElementPlus, { locale }).mount('#app');
-// 可以给 preload 进程发消息
-// postMessage({ payload: 'removeLoading' }, '*')
