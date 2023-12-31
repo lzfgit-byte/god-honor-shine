fb223a9d0bec60b452385b34be7307efeb95acd8
lzfgit-byte <1807409742@qq.com>
2023-12-31 20:14:49 +0800

    feat：img get

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,1 +1,12 @@
+import { getImgBase64 } from '../../electron/http';
 import { executeFunction } from '@/utils/ipc';
+
+/**
+ *获取html
+ * @param url
+ */
+export const f_requestHtmlByWin = async (url: string): Promise<string> =>
+  executeFunction('requestHtmlByWin', url);
+
+export const f_getImgBase64 = async (url: string): Promise<string> =>
+  executeFunction('getImgBase64', url);
