35f1ba59143fa5faf9cdf08ce3ac612c788656e7
lzfgit-byte <1807409742@qq.com>
2024-01-07 17:06:52 +0800

    feat:re by pnpm

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,9 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import { NButton, NInput, NTag, create } from 'naive-ui';
-import locale from 'element-plus/es/locale/lang/zh-cn'; // element中文
-import ElementPlus from 'element-plus';
 import App from '@/App.vue';
 import 'animate.css';
 import { registerRouter } from '@/router/router';
-import 'element-plus/dist/index.css';
 const pinia = createPinia();
 
 const naive = create({
@@ -16,4 +13,4 @@ const app = createApp(App);
 app.use(pinia);
 app.use(naive);
 registerRouter(app);
-app.use(ElementPlus, { locale }).mount('#app');
+app.mount('#app');
