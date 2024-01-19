6c118497c72577de41f4c90334d0ac42a1ee9640
lzfgit-byte <1807409742@qq.com>
2024-01-19 23:28:50 +0800

    feat:re by pnpm

--- electron/controller/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './index-controller';
-export * from './html5-controller';
--- electron/preload/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * 主进程的preload
- */
-window.onload = () => {
-  console.log('load:', window.location.href);
-};
-window.onmessage = (env: any) => {
-  console.log(env);
-};
