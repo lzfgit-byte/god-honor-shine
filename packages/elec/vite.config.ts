a071661852794d8d0e44a6e9e8c6449cb7dd58f4
lzfgit-byte <1807409742@qq.com>
2024-01-20 00:06:47 +0800

    feat:re by pnpm

--- packages/elec/vite.config.ts
+++ packages/elec/vite.config.ts
@@ -58,25 +58,7 @@ export default defineConfig(({ command }) => {
           },
         },
         {
-          entry: 'electron/preload/down-load.ts',
-          onstart(options) {
-            // Notify the Renderer-Process to reload the page when the Preload-Scripts build is complete,
-            // instead of restarting the entire Electron App.
-            options.reload();
-          },
-          vite: {
-            build: {
-              sourcemap: sourcemap ? 'inline' : undefined, // #332
-              minify: isBuild,
-              outDir: 'dist-electron/preload',
-              rollupOptions: {
-                external: Object.keys('dependencies' in pkg ? pkg.dependencies : {}),
-              },
-            },
-          },
-        },
-        {
-          entry: 'electron/preload/video.ts',
+          entry: 'electron/preload/html-download.ts',
           onstart(options) {
             // Notify the Renderer-Process to reload the page when the Preload-Scripts build is complete,
             // instead of restarting the entire Electron App.
@@ -94,7 +76,6 @@ export default defineConfig(({ command }) => {
           },
         },
       ]),
-      // Use Node.js API in the Renderer-process
       renderer(),
     ],
     server:
