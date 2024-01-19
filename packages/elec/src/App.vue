41a5a499035c4207963689ad595a0266d20dc4ad
lzfgit-byte <1807409742@qq.com>
2024-01-19 23:52:52 +0800

    feat:re by pnpm

--- packages/elec/src/App.vue
+++ packages/elec/src/App.vue
@@ -1,5 +1,7 @@
-<template> a</template>
-<script setup lang="ts"></script>
+<template> {{ a }}</template>
+<script setup lang="ts">
+  import { a } from '@ghs/types';
+</script>
 
 <style lang="less">
   @import '@/styles/style';
