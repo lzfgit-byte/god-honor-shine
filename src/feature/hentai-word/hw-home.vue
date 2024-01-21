27787cfb83c16887b8ff44ac4b7500c35592e2b0
lzfgit-byte <1807409742@qq.com>
2024-01-21 16:20:20 +0800

    feat:cache

--- src/feature/hentai-word/hw-home.vue
+++ src/feature/hentai-word/hw-home.vue
@@ -1,10 +1,15 @@
 <template>
-  <div h-full w-full overflow-auto> {{ a }}</div>
+  <ViewLayout>
+    <template #body>
+      <div h-full w-full overflow-auto> {{ a }}</div>
+    </template>
+  </ViewLayout>
 </template>
 <script setup lang="ts">
   import { onMounted, ref } from 'vue';
   import { hw_f_getPageInfo } from '@/feature/hentai-word/apis/HwApis';
   import { f_request_html_get, f_win_html_get } from '@/utils/functions';
+  import ViewLayout from '@/components/layout/ViewLayout.vue';
 
   const a = ref('1');
   const init = async () => {
