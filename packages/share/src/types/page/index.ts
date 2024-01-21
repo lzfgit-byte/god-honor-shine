d8b71a05ac45cfb670a01ad1faf8391199cef04c
lzfgit-byte <1807409742@qq.com>
2024-01-21 14:02:08 +0800

    feat:cache

--- packages/share/src/types/page/index.ts
+++ packages/share/src/types/page/index.ts
@@ -11,11 +11,9 @@ export interface MainPageInfoType {
   title?: string; // 标题
   coverImg?: string; // 封面
   author?: string; // 作者
+  type?: string; // 类型
   tags?: PageTags[]; // 标签
   flatTags?: PageTags[]; // 浮动标签
-  time?: string; // 时间
   pages?: PaginationType[]; // 分页
-  num?: string; // 数量
-  type?: string; // 类型
   [T: string]: any;
 }
