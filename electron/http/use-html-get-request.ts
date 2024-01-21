3b1ad95a79a605ac3a66a4f1023480ca5e31a5bf
lzfgit-byte <1807409742@qq.com>
2024-01-21 13:36:47 +0800

    feat:cache

--- electron/http/use-html-get-request.ts
+++ electron/http/use-html-get-request.ts
@@ -3,19 +3,14 @@ import { CacheFileType } from '@ghs/share';
 import { processMessage, sendMessage } from '../utils/message';
 import { cache_exist, cache_get, cache_save } from '../utils/cache';
 import { logger } from '../utils/logger';
-/**
- *使用electron的net获取html
- * @param url
- */
-export const requestHtml = (url: string) => {
-  const suffix = CacheFileType.html;
+
+const requestFunc = (url: string, suffix: string, apply: (data: any) => any) => {
   return new Promise((resolve) => {
     if (cache_exist(url, suffix)) {
       const cache = cache_get(url, suffix);
       resolve(cache || '');
       return;
     }
-
     const request = net.request(url);
     let blob: any = Buffer.alloc(0);
     request.on('response', (response) => {
@@ -23,11 +18,11 @@ export const requestHtml = (url: string) => {
       let fileSize = +header['content-length'];
       response.on('data', (chunk) => {
         blob = Buffer.concat([blob, chunk], blob.length + chunk.length);
-        processMessage('【html】数据请求', blob.length, fileSize);
+        processMessage(`【${suffix}】数据请求`, blob.length, fileSize, url);
       });
       response.on('end', () => {
-        resolve(cache_save(url, String(blob), suffix));
-        processMessage('【html】数据请求', fileSize, fileSize);
+        resolve(cache_save(url, apply(blob), suffix));
+        processMessage(`【${suffix}】数据请求`, fileSize, fileSize, url);
         blob = null;
       });
       response.on('error', () => {
@@ -42,41 +37,21 @@ export const requestHtml = (url: string) => {
 };
 
 /**
+ *使用electron的net获取html
+ * @param url
+ */
+export const requestHtml = (url: string) => {
+  return requestFunc(url, CacheFileType.html, (blob) => String(blob));
+};
+
+/**
  * 使用electron的net获取图片base64
  * @param url
  */
 export const requestImage = (url: string) => {
-  const suffix = CacheFileType.img;
-  return new Promise((resolve) => {
-    if (cache_exist(url, suffix)) {
-      const cache = cache_get(url, suffix);
-      resolve(cache || '');
-      return;
-    }
-
-    const request = net.request(url);
-    let blob: any = Buffer.alloc(0);
-    request.on('response', (response) => {
-      const header = response.headers;
-      let fileSize = +header['content-length'];
-      response.on('data', (chunk) => {
-        blob = Buffer.concat([blob, chunk], blob.length + chunk.length);
-        processMessage('received:', blob.length, fileSize, url);
-      });
-      response.on('end', () => {
-        const base64Image = `data:image/png;base64,${Buffer.from(blob).toString('base64')}`;
-        cache_save(url, base64Image, suffix);
-        resolve(base64Image);
-        processMessage('received:', fileSize, fileSize, url);
-        blob = null;
-      });
-      response.on('error', () => {
-        logger.db_error('请求失败', url);
-      });
-    });
-    request.on('error', () => {
-      sendMessage('request error', 'error');
-    });
-    request.end();
-  });
+  return requestFunc(
+    url,
+    CacheFileType.img,
+    (blob) => `data:image/png;base64,${Buffer.from(blob).toString('base64')}`
+  );
 };
