24d4ebff0420eed01a6b74491764f5bc7c25e3be
lzfgit-byte <1807409742@qq.com>
2024-01-21 13:50:03 +0800

    feat:cache

--- electron/controller/html5-controller.ts
+++ electron/controller/html5-controller.ts
@@ -4,10 +4,27 @@ import { getImgBase64, requestHtml, requestHtmlByWin, requestImage } from '../ht
  * 默认使用electron的request
  * @param url
  */
-export const getImageBase64 = (url: string) => {
+export const request_img_get = (url: string) => {
   return requestImage(url);
 };
-
-export const getHtml = (url: string) => {
+/**
+ * 获取html
+ * @param url
+ */
+export const request_html_get = (url: string) => {
   return requestHtml(url);
+};
+/**
+ * 使用新窗口获取图片
+ * @param url
+ */
+export const win_img_get = (url: string) => {
+  return getImgBase64(url);
+};
+/**
+ * 使用新窗口获取html
+ * @param url
+ */
+export const win_html_get = (url: string) => {
+  return requestHtmlByWin(url);
 };
