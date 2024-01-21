20307ad38bc7a40f0842ed4221e5c85b753ec4c6
lzfgit-byte <1807409742@qq.com>
2024-01-21 21:32:55 +0800

    feat:image

--- src/feature/hentai-word/hw-home.vue
+++ src/feature/hentai-word/hw-home.vue
@@ -25,6 +25,7 @@
   import { onMounted, ref } from 'vue';
   import type { PageItemType, PageTags, PaginationType } from '@ghs/share';
 
+  import { nextTick } from 'vue-demi';
   import { hw_f_getPageInfo } from '@/feature/hentai-word/apis/HwApis';
   import { f_request_html_get } from '@/utils/functions';
   import ViewLayout from '@/components/layout/view-layout.vue';
@@ -36,11 +37,12 @@
   const tags = ref<PageTags[]>();
   const items = ref<PageItemType[]>();
   const load = async (url = 'https://thehentaiworld.com/?new') => {
+    const html = await f_request_html_get(url);
+    const mainPage = await hw_f_getPageInfo(html);
     pagination.value = [];
     items.value = [];
     tags.value = [];
-    const html = await f_request_html_get(url);
-    const mainPage = await hw_f_getPageInfo(html);
+    await nextTick();
     pagination.value = mainPage.pagination;
     items.value = mainPage.items;
     tags.value = mainPage.tags;
