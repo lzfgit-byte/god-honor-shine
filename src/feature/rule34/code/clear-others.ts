66e6934ed786a4dfabc845f6a9857cc810153d41
lzfgit-byte <1807409742@qq.com>
2024-01-29 19:19:58 +0800

    feat:comic reader

--- src/feature/rule34/code/clear-others.ts
+++ src/feature/rule34/code/clear-others.ts
@@ -8,6 +8,10 @@
     $('.btn_more').remove();
     $('.main').attr('style', 'padding: 10px 0 0 0;');
     $('.video_tools').attr('style', 'padding-bottom: 10px;');
+    $('.video_tools').attr('style', 'padding-bottom: 10px;');
+    const $style = document.createElement('style');
+    $style.innerHTML = ` div::-webkit-scrollbar,body::-webkit-scrollbar {width: 0;}`;
+    document.getElementsByTagName('head')[0].appendChild($style);
     resolve('aaaa');
   });
 })();
