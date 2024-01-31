6421e17b44d07609cfb57265f0ede557166b3d2f
lzfgit-byte <1807409742@qq.com>
2024-01-31 19:08:48 +0800

    feat:缓存文件修改

--- electron/const/app-paths.ts
+++ electron/const/app-paths.ts
@@ -2,6 +2,7 @@ import path from 'node:path';
 const db_dir = `${process.env.LOCALAPPDATA}\\ghs3.0`;
 const db_path = `${db_dir}\\sqlite.db`; // aka C:\Users\用户名\AppData\Local\ghs3.0
 const cache_path = path.join(process.cwd(), '\\ghs-cache\\');
+const config_path = path.join(process.cwd(), '\\config.json');
 export const APP_PATHS = {
   db_dir,
   db_path,
