b6f8711ffeabb67d30ed2c49febb50047499f92c
lzfgit-byte <1807409742@qq.com>
2024-02-01 20:02:11 +0800

    feat:收藏修改

--- src/feature/rule34/code/clear-others.ts
+++ src/feature/rule34/code/clear-others.ts
@@ -1,13 +1,21 @@
 (async () => {
   return new Promise((resolve) => {
+    // @ts-ignore
     $('body').attr('class', '');
+    // @ts-ignore
     $('.spot_under').remove();
-    $('#custom_list_videos_related_videos').remove();
+    // $('#custom_list_videos_related_videos').remove();//视频列表
+    // @ts-ignore
     $('.footer').remove();
+    // @ts-ignore
     $('.header').remove();
+    // @ts-ignore
     $('.btn_more').remove();
+    // @ts-ignore
     $('.main').attr('style', 'padding: 10px 0 0 0;');
+    // @ts-ignore
     $('.video_tools').attr('style', 'padding-bottom: 10px;');
+    // @ts-ignore
     $('.video_tools').attr('style', 'padding-bottom: 10px;');
     const $style = document.createElement('style');
     $style.innerHTML = ` div::-webkit-scrollbar,body::-webkit-scrollbar {width: 0;}`;
