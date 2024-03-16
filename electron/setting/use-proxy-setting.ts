3bd68de4689c37ef455b0ad62387519552d24b1f
lzfgit-byte <1807409742@qq.com>
2024-03-16 16:28:02 +0800

    fix:new feature

--- electron/setting/use-proxy-setting.ts
+++ electron/setting/use-proxy-setting.ts
@@ -1,14 +1,13 @@
 import type { BrowserWindow } from 'electron';
 import { sendMessage } from '../utils/message';
 import useSystemSetting from './use-system-setting';
+// let ignore = ['bobolj.com'];
+// for (let i = 0; i < ignore.length; i++) {
+//   if (url.indexOf(ignore[i]) > -1) {
+//     return 'DIRECT';
+//   }
+// }
 function FindProxyForURL(url, host) {
-  let ignore = ['bobolj.com'];
-  for (let i = 0; i < ignore.length; i++) {
-    if (url.indexOf(ignore[i]) > -1) {
-      return 'DIRECT';
-    }
-  }
-
   return `PROXY $proxyHttp`;
 }
 const pacScript = (proxyHttp: string) =>
