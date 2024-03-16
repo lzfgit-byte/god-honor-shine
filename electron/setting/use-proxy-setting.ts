2a3d00af02aa33480882cf2b03934524b742b581
lzfgit-byte <1807409742@qq.com>
2024-03-16 16:48:24 +0800

    fix:new feature

--- electron/setting/use-proxy-setting.ts
+++ electron/setting/use-proxy-setting.ts
@@ -3,16 +3,27 @@ import { sendMessage } from '../utils/message';
 import useSystemSetting from './use-system-setting';
 // @ts-ignore
 import pac from './pac?url';
+function FindProxyForURL(url, host) {
+  let ignore = ['bobolj.com'];
+  for (let i = 0; i < ignore.length; i++) {
+    if (url.indexOf(ignore[i]) > -1) {
+      return 'DIRECT';
+    }
+  }
+  return `PROXY $proxyHttp`;
+}
+
 const pacScript = (proxyHttp: string) =>
-  `data:text/plain;base64,${Buffer.from(
+  `data:video/mp2t;base64,${Buffer.from(
     FindProxyForURL.toString()?.replace('$proxyHttp', proxyHttp),
     'utf8'
   ).toString('base64')}`;
+
 const doJob = async (win: BrowserWindow) => {
   const { proxy, needProxy, proxyHttp } = await useSystemSetting();
   if (needProxy && proxy) {
     await win.webContents.session.setProxy({
-      pacScript: pac,
+      pacScript: pacScript(proxyHttp),
     });
   }
 };
