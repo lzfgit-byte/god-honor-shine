918b10aa2e081ca51a30de5baba4f2be20d92632
lzfgit-byte <1807409742@qq.com>
2024-03-16 17:10:50 +0800

    fix:new feature

--- electron/setting/use-proxy-setting.ts
+++ electron/setting/use-proxy-setting.ts
@@ -1,29 +1,24 @@
 import type { BrowserWindow } from 'electron';
-import { sendMessage } from '../utils/message';
 import useSystemSetting from './use-system-setting';
-// @ts-ignore
-import pac from './pac?url';
-function FindProxyForURL(url, host) {
-  let ignore = ['bobolj.com'];
+let func = `function FindProxyForURL(url, host) {
+  let ignore = ["bobolj.com"];
   for (let i = 0; i < ignore.length; i++) {
     if (url.indexOf(ignore[i]) > -1) {
-      return 'DIRECT';
+      return "DIRECT";
     }
   }
-  return `PROXY 127.0.0.1:10809`;
-}
+  return "PROXY $proxyHttp";
+}`;
 
 const doJob = async (win: BrowserWindow) => {
   const { proxy, needProxy, proxyHttp } = await useSystemSetting();
-  const pacUrl = `data:video/mp2t;base64,${Buffer.from(FindProxyForURL.toString(), 'utf8').toString(
-    'base64'
-  )}`;
-  console.log(FindProxyForURL.toString()?.replace('$proxyHttp', proxyHttp));
-  console.log(pacUrl);
-  console.log(pac);
+  const pacUrl = `data:video/mp2t;base64,${Buffer.from(
+    func.replace('$proxyHttp', proxyHttp),
+    'utf8'
+  ).toString('base64')}`;
   if (needProxy && proxy) {
     await win.webContents.session.setProxy({
-      pacScript: pac,
+      pacScript: pacUrl,
     });
   }
 };
