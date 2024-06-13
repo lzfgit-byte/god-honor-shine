93ef75b52029a9899726c1ed486d61f7a0c2f9e5
lzfgit-byte <1807409742@qq.com>
2024-06-13 22:34:42 +0800

    feat:init

--- electron/business/webconfigs/rule34-code-webconfiig.ts
+++ electron/business/webconfigs/rule34-code-webconfiig.ts
@@ -40,38 +40,58 @@ const ElementTypes = {
 const webConfig: WebConfig = /* break */ {
   key: 'rule34',
   name: 'r34',
-  favicon: 'https://thehentaiworld.com/favicon.ico',
-  homeUrl: 'https://thehentaiworld.com/?new',
-  searchUrl: 'https://thehentaiworld.com/?s=',
-  imgWidth: '220px',
-  imgHeight: '147px',
+  favicon: 'https://rule34video.com/favicon.ico',
+  homeUrl: 'https://rule34video.com',
+  searchUrl: 'https://rule34video.com/search/',
+  imgWidth: '270px',
+  imgHeight: '150px',
 
   drawWidth: '780px',
   historyRemember: 100,
 
-  setTags: ['标签', '收藏', '历史', '过滤选项', '系统配置', '日志'],
+  setTags: ['收藏', '历史', '过滤选项', '系统配置', '日志'],
 
   getUrlReplace($) {
     return [];
   },
   getItems($) {
     const res = [];
-    $('#thumbContainer .thumb').each((i, el) => {
+    const ids = [
+      '#custom_list_videos_most_recent_videos',
+      '#custom_list_videos_latest_videos_list',
+      '#custom_list_videos_videos_list_search',
+    ];
+    let list = null;
+    ids.forEach((item) => {
+      if (list === null || list?.length === 0) {
+        list = $(`${item} .thumbs > div.item.thumb`);
+      }
+    });
+    list.each((i, el) => {
       const $el = $(el);
-      const $a = $el.find(ElementTypes.a);
-      const $h4 = $a.find(ElementTypes.h4);
+      const $a = $el.find('a.js-open-popup');
       const $img = $a.find(ElementTypes.img);
-      const $span = $a.find('span');
-      const title = helpElAttr($a, ElementAttr.title);
-      const coverImg = helpElAttr($img, ElementAttr.src);
+      const $title = $a.find('div.thumb_title');
+      // tags
+      const $quality = $a.find('div.quality');
+      const $time = $a.find('div.time');
+      const $added = $a.find('div.thumb_info div.added');
+      const $views = $a.find('div.thumb_info div.views');
+
+      const title = helpElText($title);
+      const coverImg = helpElAttr($img, ElementAttr.dataWebp);
       const jumpUrl = helpElAttr($a, ElementAttr.href);
-      const flatTags = [{ title: helpElText($h4) }, { title: helpElText($span) }];
-      const isVideo = flatTags.some((item) => item.title.toUpperCase() === 'VIDEO');
+      const flatTags = [
+        { title: helpElText($quality) },
+        { title: helpElText($time) },
+        { title: helpElText($added) },
+        { title: helpElText($views) },
+      ];
       res.push({
         jumpUrl,
         title,
         coverImg,
-        type: isVideo ? 'video' : 'image',
+        type: 'video',
         renderType: 'normal',
         tags: flatTags.filter((item) => item.title),
       });
@@ -82,6 +102,59 @@ const webConfig: WebConfig = /* break */ {
 
   getPagination($) {
     const res = [];
+    const ids = [
+      '#custom_list_videos_most_recent_videos_pagination',
+      '#custom_list_videos_latest_videos_list_pagination',
+      '#custom_list_videos_videos_list_search_pagination',
+    ];
+    let $more = null;
+    ids.forEach((item) => {
+      if ($more === null || $more?.length === 0) {
+        $more = $(`${item} .item`);
+      }
+    });
+    $more.each((i, el) => {
+      // li标签
+      const $el = $(el);
+      if ($el.hasClass('jump_to')) {
+        return;
+      }
+      const $a = $el.find('a');
+      //
+      let title = $el.hasClass('next') ? 'next' : $el.hasClass('prev') ? 'prev' : helpElText($a);
+      title = title.replace(/\n/g, '').trim();
+      let url = helpElAttr($a, 'href');
+      if (url === '#search') {
+        const dataParameters = helpElAttr($a, 'data-parameters');
+        const arr1 = dataParameters.split(';');
+        let qStr = 'mode:async&function:get_block&block_id:custom_list_videos_videos_list_search';
+        let qValue = '';
+        arr1.forEach((item) => {
+          const arr2 = item.split(':');
+          if (arr2.length === 2) {
+            const [key, value] = arr2;
+            const arr3 = key.split('+');
+            if (arr3.length > 0) {
+              arr3.forEach((tm) => {
+                qStr += `${tm}=${value}&`;
+              });
+            } else {
+              qStr += `${key}=${value}&`;
+            }
+
+            if (key === 'q') {
+              qValue = value;
+            }
+          }
+        });
+        url = `https://rule34video.com/search/${qValue}/?${qStr.substring(0, qStr.length - 1)}`;
+      }
+      if (!url.startsWith('https://rule34video.com')) {
+        url = `https://rule34video.com${url}`;
+      }
+      const isCurrent = $el.hasClass('active');
+      res.push({ title, isCurrent, url });
+    });
     $('#more-hentai li').each((i, el) => {
       // li标签
       const $el = $(el);
@@ -96,16 +169,7 @@ const webConfig: WebConfig = /* break */ {
     return res;
   },
   getTags($) {
-    const res = [];
-    $('#tags > li').each((i, el) => {
-      const $el = $(el);
-      const $a = $el.find('a');
-      const $span = $el.find('span');
-      const title = `${helpElText($a)} ${helpElText($span)}`;
-      const url = `https:${helpElAttr($a, ElementAttr.href)}`;
-      res.push({ title, url });
-    });
-    return res;
+    return [];
   },
   async getDetailInfo(item, cheerio) {
     const key = hashString(item.jumpUrl);
--- src/hooks/use-global-state.ts
+++ src/hooks/use-global-state.ts
@@ -1,4 +1,4 @@
-import { ref } from 'vue-demi';
+import { ref, watch } from 'vue-demi';
 import type { BaseConfig, Item, Pagination, SetTag, Tag, UrlReplace } from '@ghs/types';
 import type { CollectEntity } from '@ghs/constant';
 import { computed } from 'vue';
@@ -17,8 +17,13 @@ const loading = ref(false);
 const logs = ref([]);
 
 const drawerOpen = ref(false);
-const segmentedValue = ref<SetTag>('标签');
+const segmentedValue = ref<SetTag>();
 const segmentedData = computed(() => webConfig?.value?.setTags);
+watch(segmentedData, () => {
+  if (segmentedData.value.length > 0) {
+    segmentedValue.value = segmentedData.value[0];
+  }
+});
 
 const collects = ref<CollectEntity[]>();
 
