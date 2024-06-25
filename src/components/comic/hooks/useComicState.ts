12c8d5f48a427b983602d538552fdeea90ec96c5
18074 <1807409742@qq.com>
2024-06-25 20:46:49 +0800

    feat:init

--- src/components/comic/hooks/useComicState.ts
+++ src/components/comic/hooks/useComicState.ts
@@ -3,9 +3,9 @@ import type { CComic, CContent } from '@ghs/types';
 import { message } from 'ant-design-vue';
 import { isString } from '@ilzf/utils';
 import type { ComicHistory } from '@ghs/constant';
+import { useScroll } from '@vueuse/core';
 import useGlobalState from '@/hooks/use-global-state';
 import { f_getComicIImages, f_getContent, f_getCurrentContentUrl } from '@/utils/business';
-
 export default (url: string) => {
   const { logs } = useGlobalState();
   const containerRef = ref<HTMLDivElement>();
@@ -13,6 +13,7 @@ export default (url: string) => {
   const comicImages = ref<CComic[]>([]);
   const drawValue = ref(true);
   const currentContent = ref<ComicHistory>();
+
   const loadContent = async () => {
     contents.value = await f_getContent(url);
   };
@@ -27,10 +28,15 @@ export default (url: string) => {
     }
     comicImages.value = await f_getComicIImages(url);
     currentContent.value = await f_getCurrentContentUrl();
+    containerRef.value.scrollTo({ top: 0, behavior: 'smooth' });
   };
+  const { x, y, isScrolling, arrivedState, directions } = useScroll(containerRef);
   onMounted(async () => {
     await loadContent();
     currentContent.value = await f_getCurrentContentUrl();
+    if (currentContent.value) {
+      await getImages(currentContent.value.contentUrl);
+    }
   });
   return { containerRef, contents, comicImages, getImages, drawValue, currentContent };
 };
