3bf038dda562c53c21660951ffb76f19cf56f5ed
18074 <1807409742@qq.com>
2024-09-28 19:14:56 +0800

    feat:init

--- electron/server/hook/useFileDispatch.ts
+++ electron/server/hook/useFileDispatch.ts
@@ -1,5 +1,5 @@
 import { join } from 'node:path';
-import { readFileSync } from 'node:fs';
+import fs, { readFileSync } from 'node:fs';
 import type { Request, Response } from 'express-serve-static-core';
 
 export default async (route: string, req: Request, res: Response) => {
@@ -9,14 +9,18 @@ export default async (route: string, req: Request, res: Response) => {
     return res.end(html);
   }
   if (route === '*') {
-    const filePath = join(`${process.env.DIST}h5/`, req.url);
+    const filePath = join(`${process.env.DIST}`, 'h5', req.url);
     if (filePath.indexOf('.js') > -1) {
       res.setHeader('Content-Type', 'text/javascript');
     }
     if (filePath.indexOf('.css') > -1) {
       res.setHeader('Content-Type', 'text/css');
     }
-    const fileData = readFileSync(filePath, 'utf-8');
-    return res.end(fileData);
+    if (fs.existsSync(filePath)) {
+      const fileData = readFileSync(filePath, 'utf-8');
+      res.end(fileData);
+    } else {
+      res.end(filePath);
+    }
   }
 };
